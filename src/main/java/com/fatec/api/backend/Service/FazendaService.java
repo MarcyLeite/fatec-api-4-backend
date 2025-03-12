95dfd408a0cfdf0d05f1720dd1da8d62db80a433
YanYamim <yanyamim@gmail.com>
2025-03-12 16:13:37 +0000

    feat(Controller e Service) Criação do Controller e o Service para o endpoint de listagem de fazendas paginadas

---
+++ src/main/java/com/fatec/api/backend/Service/FazendaService.java
@@ -0,0 +1,26 @@
+package com.fatec.api.backend.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.fatec.api.backend.Entity.Fazenda;
+import com.fatec.api.backend.Repository.FazendaRepository;
+
+@Service
+public class FazendaService {
+    
+    private FazendaRepository fazendaRepository;
+
+    @Autowired
+    public FazendaService(FazendaRepository fazendaRepository) {
+        this.fazendaRepository = fazendaRepository;
+    }
+
+    public Page<Fazenda> listarFazendasPaginadas(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return fazendaRepository.findAll(pageable);
+    }
+}
