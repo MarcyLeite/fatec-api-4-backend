22bedd1950efb93b85c3b18525dd34e6d07feb3d
YanYamim <yanyamim@gmail.com>
2025-03-24 13:31:29 +0000

    test(#12) Refazendo os testes devido a refatoração da estrutura

--- src/test/java/com/fatec/api/backend/service/EstadoServiceTest.java
+++ src/test/java/com/fatec/api/backend/service/EstadoServiceTest.java
@@ -1,54 +1,69 @@
 package com.fatec.api.backend.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.fatec.api.backend.DTO.EstadoDTO;
 import com.fatec.api.backend.model.Estado;
 import com.fatec.api.backend.repository.EstadoRepository;
 
-@SpringBootTest
-@ActiveProfiles("test")
 public class EstadoServiceTest {
     
-    @Autowired
+    @InjectMocks
     private EstadoService estadoService;   
 
-    @Autowired
+    @Mock
     private EstadoRepository estadoRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void deveRetornarComSucessoListaDeEstados() {
         Estado estado1 = new Estado();
+        estado1.setId(1L);
         estado1.setNome("São Paulo");
-        estado1.setSigla("SP");
 
         Estado estado2 = new Estado();
+        estado2.setId(2L);
         estado2.setNome("Minas Gerais");
-        estado2.setSigla("MG");
 
-        estadoRepository.saveAll(List.of(estado1, estado2));
-
-        List<Estado> resultado = estadoService.listarEstados();
+        when(estadoRepository.findEstadoByIdAndName()).thenReturn(
+            List.of(new EstadoDTO(1L, "São Paulo"), new EstadoDTO(2L, "Minas Gerais"))
+        );
+        
+        List<EstadoDTO> resultado = estadoService.listarEstados();
 
         assertEquals(2, resultado.size());
-        assertEquals("São Paulo", resultado.get(0).getNome());
-        assertEquals("SP", resultado.get(0).getSigla());
-        assertEquals("Minas Gerais", resultado.get(1).getNome());
-        assertEquals("MG", resultado.get(1).getSigla());
+        assertEquals(1L, resultado.get(0).id());
+        assertEquals("São Paulo", resultado.get(0).nome());
+        assertEquals(2L, resultado.get(1).id());
+        assertEquals("Minas Gerais", resultado.get(1).nome());
+
+        verify(estadoRepository, times(1)).findEstadoByIdAndName();
     }
 
     @Test
     void deveFalharAoListarEstados() {
-        estadoRepository.deleteAll();
+       when(estadoRepository.findEstadoByIdAndName()).thenReturn(List.of());
 
-        List<Estado> resultado = estadoService.listarEstados();
+        List<EstadoDTO> resultado = estadoService.listarEstados();
 
         assertEquals(0, resultado.size());
+
+        verify(estadoRepository, times(1)).findEstadoByIdAndName();
+
     }
 }
