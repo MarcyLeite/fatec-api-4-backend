c838ad0ecc359cb2b2a31d8ae9e92b16a5222b72
YanYamim <yanyamim@gmail.com>
2025-04-21 17:22:24 +0000

    feat(#30): Implementação do método post para cadastro de usuário

--- src/main/java/com/fatec/api/backend/service/UsuarioService.java
+++ src/main/java/com/fatec/api/backend/service/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.fatec.api.backend.model.Usuario;
+import com.fatec.api.backend.model.Usuario.Role;
 import com.fatec.api.backend.repository.UsuarioRepository;
 
 @Service
@@ -18,5 +19,14 @@ public class UsuarioService {
 
     public List<Usuario> listarAnalistas() {
         return usuarioRepository.findByRole(Usuario.Role.Analista);
+    }
+
+    public Usuario cadastrarUsuario(String nome, String email, Role role) {
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setNome(nome);
+        novoUsuario.setEmail(email);
+        novoUsuario.setRole(role);
+        Usuario usarioSalvo = usuarioRepository.save(novoUsuario);
+        return usarioSalvo;
     }
 }
